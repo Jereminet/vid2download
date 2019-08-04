e5ad6be64f25ef42ac5ca1024d3e15a6f2b46fdd
Jereminet <jeremy.minet@student.uclouvain.be>
2019-08-04 21:02:02 +0200

    first test

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 from tkinter import *
 
+root = Tk()
+
+one = Label(root, text="Option", bg="red", fg="blue")
+one.grid(row=0)
+
+entry1 = Entry(root)
+entry1.grid(row=1,column=1)
+
+root.mainloop()
